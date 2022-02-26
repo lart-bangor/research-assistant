ceac2bc361000fdde779a51dde3dbd145ec4107f
Florian <florian@florian.me.uk>
2022-02-26 00:33:31 +0000

    Abstraction of data schema base class

--- lart_survey_client/datavalidator/schemas.py
+++ lart_survey_client/datavalidator/schemas.py
@@ -1,19 +1,21 @@
 """Prototypes to conveniently define data classes with built-in validation."""
-from abc import ABC, abstractmethod
 from typing import Union, Optional, Any, Callable
-from .types import PatternT
 import re
 
 DataFieldDescT = Union[
-    "DataField",    # Fully instantiated DataField
-    dict[str, Any]  # For shorthand constructor notations
+    "DataField",  # Fully instantiated DataField
+    dict[str, Any],  # For shorthand constructor notations
 ]
 
+DataGroupDataT = dict[str, DataFieldDescT]
+
 DataGroupDescT = Union[
-    "DataGroup",                                            # Fully instantiated DataGroup
-    dict[str, Union[DataFieldDescT, "DataGroupDescT"]],     # shorthand constructor
-    list[Union["DataField", "DataGroup"]]                   # list of full instances
+    "DataGroup", DataGroupDataT  # Fully instantiated DataGroup  # shorthand constructor
 ]
+
+SchemaDescT = dict[str, Union[DataGroupDescT, DataFieldDescT]]
+
+SchemaDataT = dict[str, Union[dict[str, Any], Any]]
 
 
 class DataField:
@@ -23,9 +25,24 @@ class DataField:
     declaration.
     """
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, type_: Any, typedesc: str, required: bool = True):
         """Instantiates a new DataField."""
-        self.name = name
+        self.name: str = name
+        self.type_: Any = type_
+        self.typedesc: str = typedesc
+        self.required: bool = required
+
+    def __str__(self) -> str:
+        """Returns a (possibly shortened) string representaiton of the DataField."""
+        label = self.__class__.__name__
+        return (
+            f"{label}({repr(self.name)}, {self.type_}, "
+            f"{repr(self.typedesc)}, required={self.required})"
+        )
+
+    def __repr__(self) -> str:
+        """Returns a Pythonesque string representation of the DataField."""
+        return self.__str__()
 
 
 class CField(DataField):
@@ -36,16 +53,26 @@ class CField(DataField):
         name: str,
         type_: str,
         typedesc: str,
-        validationmethod: Union[str, Callable[[Any], Any]]
+        vmethod: Union[str, Callable[[Any], Any]],
+        required: bool = True,
     ):
         """Instantiates a new CField."""
-        pass
+        super().__init__(name, type_, typedesc, required)
+        self.vmethod: Union[str, Callable[[Any], Any]] = vmethod
 
+    def __repr__(self) -> str:
+        """Returns a Pythonesque string representation of the CField."""
+        label = self.__class__.__name__
+        params = {
+            "name": repr(self.name),
+            "type_": repr(self.type_),
+            "typedesc": repr(self.typedesc),
+            "vmethod": repr(self.vmethod),
+            "required": repr(self.required),
+        }
+        paramspec = ", ".join(f"{k}={v}" for k, v in params.items())
+        return f"{label}({paramspec})"
 
-class CFieldList(CField):
-    """Defines a field containing an arbitrary number of CField data."""
-
-    pass
 
 class VField(DataField):
     """Defines an auto-validated data field for DataSchema classes."""
@@ -55,22 +82,47 @@ class VField(DataField):
         name: str,
         type_: str,
         typedesc: str,
-        constraint: PatternT,
+        constraint: Any,
         forcecast: Optional[bool] = None,
         ignorecase: Optional[bool] = None,
         flags: Union[re.RegexFlag, int] = 0,
+        required: bool = True,
     ):
         """Instantiates a new VField."""
-        self.name = name,
-        self.type_ = type_,
-        self.typedesc = typedesc,
-        self.constraint = constraint,
-        self.forcecast = forcecast,
-        self.ignorecase = ignorecase,
-        self.flags = flags
+        super().__init__(name, type_, typedesc, required)
+        self.constraint: Any = constraint
+        self.forcecast: Optional[bool] = forcecast
+        self.ignorecase: Optional[bool] = ignorecase
+        self.flags: Union[re.RegexFlag, int] = flags
+
+    def __repr__(self) -> str:
+        """Returns a Pythonesque string representation of the VField."""
+        label = self.__class__.__name__
+        params = {
+            "name": repr(self.name),
+            "type_": repr(self.type_),
+            "typedesc": repr(self.typedesc),
+            "constraint": repr(self.constraint),
+            "forcecast": repr(self.forcecast),
+            "ignorecase": repr(self.ignorecase),
+            "flags": repr(self.flags),
+            "required": repr(self.required),
+        }
+        paramspec = ", ".join(f"{k}={v}" for k, v in params.items())
+        return f"{label}({paramspec})"
 
 
-class VFieldList(VField):
+class DataFieldList(DataField):
+    """Defines a field containing an arbitrary number of DataField data."""
+
+
+class CFieldList(CField, DataFieldList):
+    """Defines a field containing an arbitrary number of CField data."""
+
+    pass
+
+
+class VFieldList(VField, DataFieldList):
     """Defines a field containing an arbitrary number of VField data."""
 
     pass
@@ -79,34 +131,152 @@ class VFieldList(VField):
 class DataGroup:
     """Defines a data group for DataSchema classes."""
 
-    def __init__(
-        self,
-        name: str,
-        fields: DataGroupDescT
-    ):
+    def __init__(self, name: str, fields: DataGroupDataT):
         """Instantiates a new DataGroup."""
-        self.name = name
-        self.fields = fields
+        self.name: str = name
+        self.fields: DataGroupDataT = fields
+
+    def __str__(self) -> str:
+        """Returns a (possibly shortened) string representaiton of the DataGroup."""
+        label = self.__class__.__name__
+        fieldkeys = list(self.fields.keys())
+        if len(fieldkeys) > 3:
+            fieldkeys = fieldkeys[:3]
+            fieldkeys.append("...")
+        paramspec = ", ".join(fieldkeys)
+        return f"{label}(name={self.name}, fields=({paramspec}))"
+
+    def __repr__(self) -> str:
+        """Returns a Pythonesque string representation of the DataGroup."""
+        label = self.__class__.__name__
+        paramspec = ", ".join(f"{k}={repr(v)}" for k, v in self.fields.items())
+        return f"{label}(name={self.name}, {{{paramspec}}})"
 
 
-class DataSchema(ABC):
-    """Abstract base class to define auto-validating data classes."""
+class DataSchema:
+    """Abstract base class to define auto-validating data classes.
+    
+    @TODO:  - __getattr__(self, name) - to retreive data
+            - __setattr__(self, name, value) - to set data (with validation)
+            - __delattr__(self, name) - to remove/clear a datapoint
+            - Move some __new__ stuff to __init_subclass__(cls)?
+            - Functions to set and validate data by group (inject to getattr()?)
+            - JSON import/export
+    """
 
-    __schema: DataGroupDescT
+    __schematized: bool
+    __data: SchemaDataT
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args: Any, **kwargs: Any):  # noqa: C901
         """Constructs a new DataSchema instance."""
+        privateref = f"_{cls.__name__}__schema"
+        if not hasattr(cls, privateref):
+            raise AttributeError(
+                f"Instance of {cls.__name__} must define a class attribute __schema."
+            )
+        cls.__schema: SchemaDescT = getattr(cls, privateref)
+        privateref = f"_{cls.__name__}__schematized"
+        if hasattr(cls, privateref):
+            cls.__schematized = getattr(cls, privateref)
+        if not hasattr(cls, "__schematized") or not cls.__schematized:
+            for key in cls.__schema:
+                cls.__schema[key] = cls.__schematize(cls.__schema[key], key)
+            cls.__schematized = True
         self = super().__new__(cls)
-        self.__constructschema(cls.__schema)
+        privateref = f"_{cls.__name__}__data"
+        if not hasattr(self, privateref):
+            setattr(self, privateref, self.__materialize())
+            self.__data = getattr(self, privateref)
         return self
 
+    def iscomplete(self, onlyrequired: bool = True) -> bool:
+        """Checks whether the dataset is complete."""
+        return not bool(self.getmissing(onlyrequired))
+
+    def getmissing(self, onlyrequired: bool = True) -> SchemaDataT:  # noqa: C901
+        """Return a schematic dict with field schemas for missing fields.
+
+        @BUG: Should check for DataFieldList instances not just whether the name
+        is there or not, but also whether the list is empty or not.
+        """
+        missing: SchemaDataT = {}
+        for field in self.__schema.values():
+            if isinstance(field, DataField) and field.name not in self.__data:
+                if not onlyrequired or field.required:
+                    missing[field.name] = field
+            if isinstance(field, DataGroup):
+                for subfield in field.fields.values():
+                    if (
+                        isinstance(subfield, DataField)
+                        and subfield.name not in self.__data[field.name]
+                    ):
+                        if not onlyrequired or subfield.required:
+                            if field.name not in missing:
+                                missing[field.name] = {}
+                            missing[field.name][subfield.name] = subfield
+        return missing
+
     @classmethod
-    def __constructschema(cls, schema: DataGroupDescT) -> dict[str, Any]:
+    def __schematize(  # noqa: C901
+        cls, schema: Union[DataGroupDescT, DataFieldDescT], schemaname: str
+    ) -> Union[DataGroup, DataField]:
+        # Is it an already instantiated DataField?
+        if isinstance(schema, DataField):
+            # Cannot have any subfields, so just return it..
+            return schema
+        # Is it an already instantiated DataGroup?
         if isinstance(schema, DataGroup):
-            return cls.__constructschema(schema.fields)
-        if isinstance(schema, dict):
-            data = {}
-            for name, definition in schema.items():
-                if isinstance(definition, (dict, DataField, DataGroup)):
+            # Make sure each of the DataGroup's fields is instantiated
+            for key in schema.fields:
+                if isinstance(schema.fields[key], DataField):
                     pass
+                elif isinstance(schema.fields[key], dict):
+                    tmp = cls.__schematize(schema.fields[key], key)
+                    if isinstance(tmp, DataGroup):
+                        raise RecursionError(
+                            "Not allowed to recurse in DataSchema definition."
+                        )
+                    schema.fields[key] = tmp
+                elif isinstance(schema.fields[key], DataGroup):
+                    raise RecursionError(
+                        "Not allowed to recurse in DataSchema definition."
+                    )
+            return schema
+        # Check if shorthand is a data group
+        grouptype = (dict, DataGroup, DataField)  # type: ignore
+        if all(isinstance(value, grouptype) for value in schema.values()):
+            # We have a group of items
+            fields: DataGroupDataT = {}
+            for key in schema:
+                tmp = cls.__schematize(schema[key], key)
+                if isinstance(tmp, DataGroup):
+                    raise RecursionError(
+                        "Not allowed to recurse in DataSchema definition."
+                    )
+                fields[key] = tmp
+            return DataGroup(schemaname, fields)
+        keys = schema.keys()
+        if {"type_", "typedesc", "constraint"} <= keys:
+            # It should be a VField
+            return VField(schemaname, **schema)
+        if {"type_", "typedesc", "vmethod"} <= keys:
+            # It should be a CField
+            return CField(schemaname, **schema)
+        if {"type_", "typedesc"} <= keys:
+            # A possible vanilla DataField
+            return DataField(schemaname, **schema)
+        raise TypeError("Cannot determine field type in DataScheme definition.")
+
+    @classmethod
+    def __materialize(cls) -> SchemaDataT:
+        """Creates an empty __data store pre-populated with DataGroups/DataFieldLists."""
+        data: SchemaDataT = {}
+        for field in cls.__schema.values():
+            if isinstance(field, DataGroup):
+                data[field.name] = {}
+                for subfield in field.fields.values():
+                    if isinstance(subfield, DataFieldList):
+                        data[field.name][subfield.name] = []
+            elif isinstance(field, DataFieldList):
+                data[field.name] = []
         return data
