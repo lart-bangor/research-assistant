aeca3f07cd9b3afc909f35b160abaaa22e6013e0
Marco Tamburelli <elsc1e@bangor.ac.uk>
2022-04-29 15:07:33 +0100

    updated path of consentLog.txt

--- lart_research_client/consent/__init__.py
+++ lart_research_client/consent/__init__.py
@@ -7,14 +7,19 @@ from typing import Any
 from .. import booteel
 from ..config import config
 
+data_path: Path = config.paths.data / "Consent"
+if not data_path.exists():
+    data_path.mkdir(parents=True, exist_ok=True)
 
 @eel.expose
 def record_consent(data: dict[Any, Any]):
     """Retrieve consent from partInformed.html and print to file + to console."""
     presentime = datetime.now()
     dt_string = presentime.strftime("%d/%m/%Y %H:%M:%S")
+    data_file = data_path / "consentLog.txt"
+
     try:
-        with open("lart_research_client/consent/consentLog.txt", "a") as file:
+        with open(data_file, "a") as file:
             file.write("\n>>>>CONSENT RECORDED @: ")
             file.write(dt_string + "\n")
             for key in data:
