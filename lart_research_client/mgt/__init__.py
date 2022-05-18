83f4c7b8f6848a847cd9582d410d33c1b4badaf4
Marco Tamburelli <elsc1e@bangor.ac.uk>
2022-05-18 17:03:41 +0100

    fixed json indent

--- lart_research_client/mgt/__init__.py
+++ lart_research_client/mgt/__init__.py
@@ -21,7 +21,7 @@ from playsound import playsound
  
 #retrieve initial info from index.html and print to file + to console
 @eel.expose
-def init_mgt(data: dict[Any, Any]):
+def grab_mgt_ratings(data: dict[Any, Any]):
     presentime = datetime.now()
     dt_string = presentime.strftime("%d/%m/%Y %H:%M:%S")
     #currentFileLocation = pathlib.Path(__file__).parent.absolute()
@@ -30,76 +30,25 @@ def init_mgt(data: dict[Any, Any]):
     #jsonFile = jsonLocationPath + "/web/app/mgt/versions/CymEng_ENG_GB.json"
     
     try:
-        with open("lart_research_client/mgt/data/dataLog.txt", "a") as file:
+        with open("lart_research_client/mgt/data/dataLog.json", "a") as file:
             file.write(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> NEXT >>")
             file.write("\n\nDate & Time: " + dt_string + "\n")
-            for key in data:
-                value = data[key]
-                file.write(key + ": " + str(value) + "\n")
+        
+            #file.write("\n")
+            #file.write("Presentation order: ")
+            #file.write(json.dumps(presentation_order))
+            #file.write("\n")
+            jsonOutput = json.dumps(data, indent=4)
+            file.write(jsonOutput)
+            
     except FileNotFoundError:
         print("\n")
         print("#########################################\n")
         print("#   The 'data' directory does not exist #\n")
         print("#########################################\n")
-    print("Basic info from index.html: ")
-    print(data)
-    booteel.setlocation("mgtRatings.html")
-    
-#fetches ratings from mgtRatings.html
-@eel.expose
-def grab_mgt_ratings(data: dict[Any, Any], source, version):
-    fetchCurrentGuise()
-#    global mgtAudioList, guise_counter
-    #presentation_order = key_list(data)        #record order in which data was presented and output labels
-    data = alphabetise(data)                    #now reset data in alphabetical order ready for writing to file
-    
-    #check if there are guises left to play [they play via playGuise(), called in JS]
- #   if guise_counter < (len(mgtAudioList)): ##if there are still items on the list:
-  #      mgtAudioList.pop(0)                 #remove current 1st item from list
-   #     nxtLocation = "mgtRatings.html"     #and set next page to _self
-    #    guise_counter +=1                   #reset counter
-   # else:
-    #    mgtAudioList.pop(0)
-    #    nxtLocation = "mgtEnd.html"         #else, display thank you page and end MGT
-
-   # print("\n" + "mgtAudioLIst now is: " + str(mgtAudioList))
-   
-    #record user respponses for guise
-    try:
-        with open("lart_research_client/mgt/data/dataLog.txt", "a") as file:
-            #file.write("\n")
-            #file.write("Presentation order: ")
-            #file.write(json.dumps(presentation_order))
-            #file.write("\n")
-            for key in data:
-                value = data[key]
-                file.write(key + ": " + str(value) + "\n")
-            file.write("\n")
-
-    except FileNotFoundError:
-        print("!!!The 'data' directory does not exist!!!")
-    print("\nMGT ratings from " + source + ".html: ")
+    print("\nMGT ratings: ")
     print(data)
     
-    # booteel.setlocation(nxtLocation)
-
-@eel.expose
-def playGuise():
-    currentFileLocation = pathlib.Path(__file__).parent.absolute()
-    audioLocationPath = str(currentFileLocation.as_posix())
-#    audioFile = audioLocationPath + "/audio files/" + mgtAudioList[0]
-    sleep(1.5)
- #   print("\ncurrently playing...:" + mgtAudioList[0])
-#    playsound(audioFile , block = False)
-
-@eel.expose
-def fetchCurrentGuise():               #return name of guise being played, but without extension
-    global mgtAudioList
-    currentAudio = str(mgtAudioList[0])
-    extensionBegin = currentAudio.find(".")
-    guiseName = currentAudio[0:extensionBegin]
-    return guiseName
-
 
 def randomize(dictionary):
     randomized_version = {}
