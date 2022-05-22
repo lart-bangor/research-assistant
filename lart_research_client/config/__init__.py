ec15f9eceefce2bf1ceb8978cdb8a9f5514033ee
thatfloflo <florian@florian.me.uk>
2022-05-22 23:27:50 +0100

    Made config fields documentable with getdocs() method

--- lart_research_client/config/__init__.py
+++ lart_research_client/config/__init__.py
@@ -4,10 +4,10 @@ import json
 import logging
 from appdirs import AppDirs
 from copy import copy
-from dataclasses import dataclass, field, fields, asdict, is_dataclass
+from dataclasses import MISSING, dataclass, field, fields, asdict, is_dataclass
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Callable, Optional, Union, get_type_hints
+from typing import Any, Callable, ClassVar, Optional, Union, get_type_hints
 
 __all__ = ["config", "Config", "_default_dirs"]
 
@@ -74,8 +74,68 @@ class DataclassDictMixin:
         return d
 
 
+class DataclassDocMixin:
+    """Mixin adding a getdocs() method to a dataclass.
+
+    This enables adding additional documentation to dataclass fields using the
+    field's *metadata* property. The following metadata properties are read by
+    the DataclassDocMixin:
+    - doc_label: A human-friendly label/short description of a field.
+    - doc_help: A human-friendly explanation of what a field does / why it's there.
+    - doc_values: A dictionary of label-value pairs, which can be used to give
+        an indication of specific values the field can take and what they mean.
+    """
+
+    def getdocs(                                                                # noqa: C901
+        self,
+        /,
+        recurse: bool = True,
+        include_undocumented: bool = False
+    ) -> tuple[list[dict[str, Any]], list[dict[str, Any]]]:
+        """Return a dictionary with documentation for the dataclass's fields.
+
+        Arguments:
+            recurse: Whether to also fetch the field-docs for fields that are
+                dataclasses.
+            include_undocumented: Whether to include fields that do not have
+                at least *doc_label* assigned in their metadata.
+
+        Returns: A two-tuple, where the first member is a list of dataclass
+            fields, the second member a list of non-dataclass fields. Each list
+            contains a dictionary with information about the field.
+        """
+        dataclasses: list[dict[str, Any]] = []
+        fields_: list[dict[str, Any]] = []
+        for field_ in fields(self):
+            field_doc = {
+                "value": getattr(self, field_.name),
+                "name": field_.name,
+                "type": field_.type
+            }
+            if "doc_label" in field_.metadata or include_undocumented:
+                field_doc["label"] = field_.metadata["doc_label"]
+            else:
+                continue
+            if "doc_help" in field_.metadata:
+                field_doc["help"] = field_.metadata["doc_help"]
+            if "doc_values" in field_.metadata:
+                field_doc["values"] = field_.metadata["doc_values"]
+            if field_.default is not MISSING:
+                field_doc["default"] = field_.default
+            if is_dataclass(field_doc["value"]):
+                if recurse and isinstance(field_doc["value"], DataclassDocMixin):
+                    field_doc["fields"] = field_doc["value"].getdocs(
+                        recurse=recurse,
+                        include_undocumented=include_undocumented
+                    )
+                dataclasses.append(field_doc)
+            else:
+                fields_.append(field_doc)
+        return (dataclasses, fields_)
+
+
 @dataclass
-class Paths(DataclassDictMixin):
+class Paths(DataclassDictMixin, DataclassDocMixin):
     """Class for configuration of App paths."""
 
     config: Path = field(default=Path(_default_dirs.user_config_dir), init=False)
@@ -93,13 +153,52 @@ class Paths(DataclassDictMixin):
 
 
 @dataclass
-class Logging(DataclassDictMixin):
+class Logging(DataclassDictMixin, DataclassDocMixin):
     """Class for Logging configuration."""
 
-    max_files: int = field(default=10)
-    default_level: int = field(default=logging.WARNING)
-    stream_format: str = field(default="{levelname}:{name}: {message}")
-    file_format: str = field(default="[{asctime} {levelname:<8} {name}] {message}")
+    max_files: int = field(
+        default=10,
+        metadata={
+            "doc_label": "Maximum number of log files to keep",
+            "doc_help": (
+                "Indicates the maximal number of log files kept. "
+                "If more log files are present on app startup, "
+                "the oldest log files are deleted."
+            ),
+        }
+    )
+    default_level: int = field(
+        default=logging.WARNING,
+        metadata={
+            "doc_label": "Default log level",
+            "doc_help": (
+                "Specifies the default log level on app startup, used"
+                " if no log level is specified with the --debug [LEVEL] "
+                "command line option."
+            ),
+            "doc_values": {
+                "Debug": 10,
+                "Info": 20,
+                "Warning": 30,
+                "Error": 40,
+                "Critical": 50.
+            },
+        }
+    )
+    stream_format: str = field(
+        default="{levelname}:{name}: {message}",
+        metadata={
+            "doc_label": "Console log message format",
+            "doc_help": "Format for log and error messages displayed on the console.",
+        }
+    )
+    file_format: str = field(
+        default="[{asctime} {levelname:<8} {name}] {message}",
+        metadata={
+            "doc_label": "File log message format",
+            "doc_help": "Format for log and error messages in the log files.",
+        }
+    )
 
     def get_stream_handler(self, stream: Any = None) -> logging.StreamHandler[Any]:
         """Return a `logging.StreamHandler` object for logging."""
@@ -142,25 +241,93 @@ class Logging(DataclassDictMixin):
 
 
 @dataclass
-class Sequences(DataclassDictMixin):
+class Sequences(DataclassDictMixin, DataclassDocMixin):
     """Class for app-task sequencing configuration."""
 
-    atolc: str = field(default="memorygame")
-    lsbqrml: str = field(default="atolc")
-    memorygame: str = field(default="")
-    consent: str = field(default="lsbqrml")
+    _sequence_options: ClassVar[dict[str, str]] = {
+        "AToL-C": "atolc",
+        "LSBQ-RML": "lsbqrml",
+        "Memory Game": "memorygame",
+        "Consent Form": "consent",
+        "App start screen": "",
+    }
+
+    atolc: str = field(
+        default="memorygame",
+        metadata={
+            "doc_label": "Task following the AToL-C",
+            "doc_values": _sequence_options,
+        }
+    )
+    lsbqrml: str = field(
+        default="atolc",
+        metadata={
+            "doc_label": "Task following the LSBQ-RML",
+            "doc_values": _sequence_options,
+        }
+    )
+    memorygame: str = field(
+        default="",
+        metadata={
+            "doc_label": "Task following the Memory Game",
+            "doc_values": _sequence_options,
+        }
+    )
+    consent: str = field(
+        default="lsbqrml",
+        metadata={
+            "doc_label": "Task following the Consent Form",
+            "doc_values": _sequence_options,
+        }
+    )
 
 
 @dataclass
-class Config(DataclassDictMixin):
+class Config(DataclassDictMixin, DataclassDocMixin):
     """Class for keeping track of App configuration data."""
 
     appname: str = field(default=_appname, init=False)
     appauthor: str = field(default=_appauthor, init=False)
-    logging: Logging = field(default=Logging())
-    paths: Paths = field(default=Paths())
-    sequences: Sequences = field(default=Sequences())
-    shutdown_delay: float = field(default=2.0)
+    logging: Logging = field(
+        default=Logging(),
+        metadata={
+            "doc_label": "Logging settings",
+            "doc_help": "Configures the app's debug and error logging."
+        }
+    )
+    paths: Paths = field(
+        default=Paths(),
+        metadata={
+            "doc_label": "App path and directory settings",
+            "doc_help": (
+                "Configures the paths used by the app for storing and reading "
+                "various files, such as data, settings, and logs."
+            )
+        }
+    )
+    sequences: Sequences = field(
+        default=Sequences(),
+        metadata={
+            "doc_label": "Task sequencing",
+            "doc_help": (
+                "Configures the automatic sequencing of tasks. If a task is "
+                "assigned a follow-up task, the user will be automatically "
+                "redirected to the follow-up task upon completion."
+            )
+        }
+    )
+    shutdown_delay: float = field(
+        default=2.0,
+        metadata={
+            "doc_label": "Shutdown delay",
+            "doc_help": (
+                "The number of seconds to wait with shutting down the app's "
+                "backend process after the last window has been closed.\n"
+                "Increasing this number slightly can help if you are running "
+                "on slow/underpowered hardware and experience crashes."
+            )
+        }
+    )
 
     def save(self, filename: str = "settings.json"):
         """Save configuration to a file."""
