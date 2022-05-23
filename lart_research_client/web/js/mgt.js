5b577b75e714433e8da5320559e313f78b888d7d
Marco Tamburelli <elsc1e@bangor.ac.uk>
2022-05-23 16:27:12 +0100

    continued work on mgt

--- lart_research_client/web/js/mgt.js
+++ lart_research_client/web/js/mgt.js
@@ -52,104 +52,12 @@ let sliderTxt = "";
 let btnTxt = "";
 let headerElement = "";
 let sliderElement = "";
-        
-function showMgt() {
-    //get values from researcher
-    const fetch_version = document.getElementById("selectMgtVersion");
-    const fetch_resId = document.getElementById("researcherId");
-    const fetch_location = document.getElementById("researchLocation");
-    const fetch_partId = document.getElementById("participantId");
-    const metaValues = [fetch_version.value, fetch_resId.value, fetch_location.value, fetch_partId.value];
-    setMeta(metaValues);    //load value on meta{} object, to keep for later
-    console.log("meta data is: ", meta);
-    
-    jsonFile = "versions/" + meta['version'] + ".json";  //set filename based on selected version
-    console.log("json file is: ", jsonFile);
-    hideInitForm();
-    fetch(jsonFile)
-        .then(response => response.json())
-        .then(data => setIntface(data))
-        .then(output => fetchMgt(output));  //pass the contents of jsonFile to output_info()
-    }
-    
-function setIntface(jsonObj) {
-    interface = jsonObj;
-    return interface;
-}
 
-function fetchMgt(data) {       
-    //displays MGT component of page
-    console.log("data from json is ", data);
-    headerElement = document.getElementById("language_header");
-    sliderElement = document.getElementById("slider_info");
-    btnElement = document.getElementById("btnNext");
-    document.getElementById("mgtBody").style.display = "block";     
-    
-    headerTxt = interface.base.header;
-    agree = interface.base.agreement;            
-    disagree = interface.base.disagreement;
-    sliderTxt = interface.base.sliderInfo;
-    sliderWarn = interface.base.sliderWarn;
-    btnTxt = interface.base.nextBtn;
-   
-    mgtAdjectives = interface.mgtItems;  //set mgtAdjectives
-    mgtAudioList = interface.mgtAudioList;   // set mgtAudioList;
-    moveToNext(mgtAdjectives, mgtAudioList);
-}
+//############################//
+//#    secondary functions   #//
+//############################//
 
-
-function moveToNext(words, audioFiles) {
-    if((typeof words === 'undefined'  || typeof audioFiles === 'undefined')) {
-        words = mgtAdjectives;
-        audioFiles = mgtAudioList;
-        }
-    audioLen = audioFiles.length;
-    if(audioLen >0) {                       //if list of audio recs is not empty
-        hidePrecedingDiv();
-        loadHeaders();
-        window.location.href = "#mgtTop";
-        console.log("in moveToNext audio list is: ", audioFiles);
-        playGuise(audioFiles[0]);  //play first item on guise list
-        console.log("Adjectives = ", words);
-        loadAdjectives(words);        
-        audioFiles.shift();
-        mgtAudioList = audioFiles    //redefine mgtAudioList after first item is removed
-        console.log("after popping array is ", mgtAudioList);
-                
-    } else {
-        //window.location.assign("mgtEnd.html");
-        formdata = lart.forms.getFormData("mgtDataForm"); 
-        console.log("form data is ", formdata);
-        const entries = Object.entries(formdata);
-        console.log("formdata now is: ", entries);
-        console.log("full JSOn object is : ", JSON.stringify(partResponses, undefined, 2));
-        console.log();
-        loadResponses(entries);
-        const fullDataset = {meta, partResponses};
-        console.log("FINAL JSOn object is : ", JSON.stringify(fullDataset, undefined, 2));
-        //console.log("full JSOn object after loading responses : ", JSON.stringify(partResponses, undefined, 2));
-        eel.grab_mgt_ratings(fullDataset)();
-        }     
-     
-}
-
-function loadResponses(array) {
-    for (let [key, value] of array) {
-        console.log(key, value);
-        for (const x of mgtInitialAudioList) {  
-        }
-    
-    }
-
-}
-
-function loadHeaders() {
-        headerElement.innerHTML =  headerTxt;
-        sliderElement.innerHTML =  " " + sliderTxt;
-        btnElement.innerHTML = btnTxt;
-
-}
-
+//sets meta values as object
 function setMeta(metaData) {
     meta['version'] = metaData[0];
     meta['Researcher id'] = metaData[1];
@@ -159,8 +67,99 @@ function setMeta(metaData) {
     meta['File ID'] = metaData[3] + "_" + idTime;
     }
 
+//hides user form before moving on to MGT proper
+    function hideInitForm() {
+    const form = document.getElementById('mgtToggleForm');
+    const btn = document.getElementById('toggle-btn');
+    form.style.display = 'none';
+    btn.style.display = 'none';
+    }
+
+//returns language-specific data for MGT interface
+    function setIntface(jsonObj) {
+    interface = jsonObj;
+    return interface;
+    }
+
+
+//hides sliders for which ratings have already been obtained
+    function hidePrecedingDiv() {
+    console.log("inside hide DIV counter is: ", counter);
+    if(counter !== 0) {
+        let previousId = "mgtRatingScale_" + String(counter-1);
+        console.log("previous rat8ng id is: ", previousId);
+        var x = document.getElementById(previousId);
+        console.log("var x = ", x);
+        x.style.display = "none";
+    }
+          
+}
+
+
+//sets headers to be kepth throughout test, based on language selected
+function loadHeaders() {
+    headerElement.innerHTML =  headerTxt;
+    sliderElement.innerHTML =  " " + sliderTxt;
+    btnElement.innerHTML = btnTxt;
+}
+
+//shuffles order of adjectives 
+function shuffle(a) {       //shuffles members of array
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+        }
+    return a;
+    }
+
+//plays audio guise
+function playGuise(audio) {
+    //console.log("Current audio path is ", audioFile);
+    console.log("current audio supposed to play is...:", audio);
+    //const audio = document.getElementById("guiseAudio");
+    //audio.play();
+
+}
+
+//derives label based on audiofile's name
+function fetchAudioLabel(filename) {
+    let guiseName = String(mgtAudioList[0]);
+    let extBegin = filename.indexOf(".mp3");
+    let currentGuiseName = guiseName.slice(0,extBegin);
+    return currentGuiseName;
+}
+
+//###############################################//
+//#             Primary functions               #//
+//###############################################//
+
+//loads results onto json-ready object for passing to Python
+/*
+for (const element of formArr) {
+    console.log("array pair = ", element);
+    console.log("object list is: ", responsesArr);
+*/
+
+function loadResults(formArr, responsesArr) {
+    for(i = 0; i < responsesArr.length; i++) {  //for each response object on teh array 
+        currentGuise = responsesArr[0];
+        console.log("guise name is: ", currentGuise["guise name"]);
+        if(element[0].includes(currentGuise['guise name']) ) {           //if first element of name-rating pair
+            console.log("current element match: ", element[0])               
+        } 
+        else {
+            console.log("current element MISmatch: ", element[0])
+
+        }
+    }
+ }
+
+ 
+//displays a slider for each adjective on itemList
 function loadAdjectives() {
-    //this displays a slider for each adjective on itemList
     let currentDiv = "mgtRatingScale_" + String(counter);
     const mgtElement = document.getElementById(currentDiv);
     currentGuiseName = fetchAudioLabel(mgtAudioList[0]);
@@ -194,64 +193,82 @@ function loadAdjectives() {
     }
 
 
-
-function showMgtBlock() {
-    document.getElementById("mgtBody").style.display = "block";             
-    }
-
-function hideInitForm() {
-    const form = document.getElementById('mgtToggleForm');
-    const btn = document.getElementById('toggle-btn');
-    form.style.display = 'none';
-    btn.style.display = 'none';
-    }
-
-function playGuise(audio) {
-    //console.log("Current audio path is ", audioFile);
-    console.log("current audio supposed to play is...:", audio);
-    //const audio = document.getElementById("guiseAudio");
-    //audio.play();
-
-}
-
-function fetchAudioLabel(filename) {
-    let guiseName = String(mgtAudioList[0]);
-    let extBegin = filename.indexOf(".mp3");
-    let currentGuiseName = guiseName.slice(0,extBegin);
-    return currentGuiseName;
-}
-
-function hidePrecedingDiv() {
-    console.log("inside hide DIV counter is: ", counter);
-    if(counter !== 0) {
-        let previousId = "mgtRatingScale_" + String(counter-1);
-        console.log("previous rat8ng id is: ", previousId);
-        var x = document.getElementById(previousId);
-        console.log("var x = ", x);
-        x.style.display = "none";
-    }
-      
-}
-
-function fetchAudio() {
-    currentFile = String(window.location.pathname);
-    console.log("currentFile is: ", currentFile);
-    ext = currentFile.search("index.html");
-    console.log("ext is: " , ext);
-    currentLocation = currentFile.slice(0, ext);
-    console.log("current location is: ", currentLocation);
-    audioFile = currentLocation + "audio files/" + mgtAudioList[0];
-    console.log("audio file is: ", audioFile);
-    return audioFile;
-}
-
-function shuffle(a) {       //shuffles members of array
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
+//loads rating interface as many times as there are recorded guises to rate
+function moveToNext(words, audioFiles) {
+    if((typeof words === 'undefined'  || typeof audioFiles === 'undefined')) {
+        words = mgtAdjectives;
+        audioFiles = mgtAudioList;
         }
-    return a;
+    audioLen = audioFiles.length;
+    if(audioLen >0) {                       //if list of audio recs is not empty
+        hidePrecedingDiv();
+        loadHeaders();
+        window.location.href = "#mgtTop";
+        console.log("in moveToNext audio list is: ", audioFiles);
+        playGuise(audioFiles[0]);  //play first item on guise list
+        console.log("Adjectives = ", words);
+        loadAdjectives(words);        
+        audioFiles.shift();
+        mgtAudioList = audioFiles    //redefine mgtAudioList after first item is removed
+        console.log("after popping array is ", mgtAudioList);
+                
+    } else {
+        //window.location.assign("mgtEnd.html");
+        formdata = lart.forms.getFormData("mgtDataForm"); 
+        console.log("form data is ", formdata);
+        const entries = Object.entries(formdata);
+        console.log("inside else partResponses is :", partResponses);
+        loadResults(entries, partResponses);
+        
+        //const entries = Object.entries(formdata);
+        //console.log("formdata now is: ", entries);
+        //console.log("full JSOn object is : ", JSON.stringify(partResponses, undefined, 2));
+        //console.log();
+        //loadResponses(entries);
+        //const fullDataset = {meta, partResponses};
+        //console.log("FINAL JSOn object is : ", JSON.stringify(fullDataset, undefined, 2));
+        //console.log("full JSOn object after loading responses : ", JSON.stringify(partResponses, undefined, 2));
+        eel.grab_mgt_ratings(fullDataset)();
+        }     
+     
+}
+
+//displays MGT component of page
+function fetchMgt(data) {       
+    console.log("data from json is ", data);
+    headerElement = document.getElementById("language_header");
+    sliderElement = document.getElementById("slider_info");
+    btnElement = document.getElementById("btnNext");
+    document.getElementById("mgtBody").style.display = "block";     
+      
+    headerTxt = interface.base.header;
+    agree = interface.base.agreement;            
+    disagree = interface.base.disagreement;
+    sliderTxt = interface.base.sliderInfo;
+    sliderWarn = interface.base.sliderWarn;
+    btnTxt = interface.base.nextBtn;
+      
+    mgtAdjectives = interface.mgtItems;  //set mgtAdjectives
+    mgtAudioList = interface.mgtAudioList;   // set mgtAudioList;
+    moveToNext(mgtAdjectives, mgtAudioList);
     }
+    
+
+//gets values entered by researcher
+function showMgt() {
+    const fetch_version = document.getElementById("selectMgtVersion");
+    const fetch_resId = document.getElementById("researcherId");
+    const fetch_location = document.getElementById("researchLocation");
+    const fetch_partId = document.getElementById("participantId");
+    const metaValues = [fetch_version.value, fetch_resId.value, fetch_location.value, fetch_partId.value];
+    setMeta(metaValues);    //load value on meta{} object, to keep for later
+    console.log("meta data is: ", meta);
+    
+    jsonFile = "versions/" + meta['version'] + ".json";  //set filename based on selected version
+    console.log("json file is: ", jsonFile);
+    hideInitForm();
+    fetch(jsonFile)
+        .then(response => response.json())
+        .then(data => setIntface(data))
+        .then(output => fetchMgt(output));  //pass the contents of jsonFile to output_info()
+    }   
